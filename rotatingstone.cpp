4fefad19a2a35166169601d218f27bd02fa06fa7
Henry Chan <chamhaang04@yahoo.com>
2024-03-15 12:51:44 +0800

    functions for rotating stones

---
+++ rotatingstone.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+ create_board(){
+
+}
+
+void checkconnection(){
+
+}
+void remove_stones_after_connection(){
+
+}
+
+void regenearte_stones(){
+
+}
+
+void calculate_score_for_each_stone(){
+
+}
+void print_board(){
+
+}
+
+void print_monster(){
+
+}
+
+void print_monster_health_bar(){
+
+}
+
+
+
+int main() {
+    return 0;
+}
