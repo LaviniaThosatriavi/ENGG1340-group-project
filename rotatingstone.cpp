4072e8979256827acab3bfc7066bbed9e2564159
Tracy Chan <tracysychan@outlook.com>
2024-03-19 22:06:25 +0800

    implemented print monster health bar

--- rotatingstone.cpp
+++ rotatingstone.cpp
@@ -6,6 +6,8 @@ using namespace std;
 const int row = 5;
 const int col = 5;
 int arr[row][col];
+int monsterHealth = 90;
+const int monsterMaxHealth = 100;
 
 void create_board(){
     srand(time(0));
@@ -37,6 +39,17 @@ void print_monster(){
 }
 
 void print_monster_health_bar(){
+    const int barWidth = 20;
+    cout << "Monster Health: [";
+    double healthPercentage = static_cast<double>(monsterHealth) / monsterMaxHealth;
+    int filledBarWidth = static_cast<int>(healthPercentage * barWidth);
+    for (int i = 0; i < filledBarWidth; i++){
+        cout << "#";
+    }
+    for (int i = filledBarWidth; i < barWidth; i++) {
+        cout << "-";
+    }
+    cout << "]" << endl;
 
 }
 
@@ -44,20 +57,23 @@ void print_board(){
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             if (arr[i][j] == 1) {
-                cout << "\033[1;31m" << arr[i][j] << "\033[0m ";
+                cout << "\033[1;41m" << " " << "\033[0m ";
             } else if (arr[i][j] == 2) {
-                cout << "\033[1;32m" << arr[i][j] << "\033[0m ";
+                cout << "\033[1;42m" << " " << "\033[0m ";
             } else if (arr[i][j] == 3) {
-                cout << "\033[1;33m" << arr[i][j] << "\033[0m ";
+                cout << "\033[1;43m" << " "<< "\033[0m ";
             } else if (arr[i][j] == 4) {
-                cout << "\033[1;34m" << arr[i][j] << "\033[0m ";
+                cout << "\033[1;44m" << " " << "\033[0m ";
             }
         }
+        cout << endl;
         cout << endl;
     }
 }
 
 int main() {
+    print_monster();
+    print_monster_health_bar();
     create_board();
     print_board();
     return 0;
