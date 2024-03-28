6a1a87301330b8082c641e2d63a5740dda9146ff
Henry Chan <chamhaang04@yahoo.com>
2024-03-28 22:19:13 +0800

    added some comments and the move stone function

--- rotatingstone.cpp
+++ rotatingstone.cpp
@@ -14,16 +14,81 @@ int arr[row][col];
 int monsterHealth = 90;
 const int monsterMaxHealth = 100;
 
-void create_board(){
+void create_board(){// tracy 
     srand(time(0));
     for (int i=0; i< row; i++)
         for (int j=0; j< col; j++)
             arr[i][j] = 1 + (rand() % 4);
 }
 
-void checkconnection(){
-
+void prompt_user_move(){ //henry
+    // get the user move and feed into move stones 
 }
+void move_stones(char move,int s_row,int s_col){
+    //this function basically move the stone according to the user selected move and selected stone
+    //input : move(wasd) , s_row(starting row ),s_col(starting column)
+    //output: update the board base on the user movement 
+
+
+    int start_stone= arr[s_row][s_col];// this is the starting stone being selected to connect
+    int current_stone=start_stone; // this is to keep track of the current stone being connected
+    int c_row=s_row;// the current row is the starting row
+    int c_col=s_col;// the current column is the strating column
+
+    while(true){
+        // check out of range 
+    if (move=='w'){//when stone is connected upward
+        //later need to add whether the move is valid 
+        int next_stone = arr[c_row-1][c_col];// get the stone to be connected with
+        if (next_stone==current_stone){// check whether the 2 are the same type 
+            c_row=c_row-1;
+        }
+        else{
+            break ;// if the stones are not the same type break out of the loop 
+        }
+    }
+
+    else if (move=='a'){// when stone being moved to left
+        int next_stone = arr[c_row][c_col-1];// get the stone to be connected with
+        if (next_stone==current_stone){// check whether the 2 are the same type 
+            c_col=c_col-1;
+        }
+        else{
+            break ;// if the stones are not the same type break out of the loop 
+        }
+       
+    }
+
+    else if (move=='s'){// when stone being moved to downward
+        int next_stone = arr[c_row+1][c_col];// get the stone to be connected with
+        if (next_stone==current_stone){// check whether the 2 are the same type 
+            c_row=c_row+1;
+        }
+        else{
+            break ;// if the stones are not the same type break out of the loop 
+        }
+    }
+
+    else if (move =='d'){// when stone being mooved to right 
+        int next_stone = arr[c_row][c_col+1];// get the stone to be connected with
+        if (next_stone==current_stone){// check whether the 2 are the same type 
+            c_col=c_col+1;
+        }
+        else{
+            break ;// if the stones are not the same type break out of the loop 
+        }
+    }
+
+    else{
+        cout<<"move not valid";
+    }
+    
+    // return a vector for the print board function 
+    // update board 
+    // print the board out 
+}
+}
+
 
 void remove_stones_after_connection(){
 
@@ -34,7 +99,10 @@ void regenearte_stones(){
 }
 
 void calculate_score_for_each_stone(){
-
+    // stone type 1 : refill health bar 
+    // stone type 2: skip a round of monster attack
+    // stone type 3 : normal attack 
+    // stone type 4 : double the next attack 
 }
 
 void print_monster(){
