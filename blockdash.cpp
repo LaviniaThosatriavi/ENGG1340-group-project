ed92b5013974f291038d5cf185e3a389d865212b
amxxch <91817007+amxxch@users.noreply.github.com>
2024-04-10 14:35:15 +0800

    Update blockdash.cpp: function to store the text file map into the array

--- blockdash.cpp
+++ blockdash.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+
+const int MapHeight = 17;
+// store each line of the map in the array
+array<string, MapHeight> createArrMap(string fileName)
+{
+    // Open the map file for reading
+    ifstream mapFile(fileName);
+
+    // Create an array to store the map data
+    array<string, MapHeight> mapData;
+
+    // each line of the map file
+    string line;
+
+    // Check if the map file is open
+    if (mapFile.is_open())
+    {
+        // Read each line of the map file and store it in the mapData array
+        for (int i = 0; i < MapHeight && getline(mapFile, line); i++)
+        {
+            mapData[i] = line;
+        }
+    }
+    else
+    {
+        // Print an error message if the map file cannot be opened
+        cout << "Cannot open the Map file" << endl;
+    }
+
+    return mapData; // return the map array that contains each line of the map
+}
